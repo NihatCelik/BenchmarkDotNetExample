6ca2c108d1315105edadad2352f6a01fea36498f
Nihat Çelik <nc.nihatcelik@hotmail.com>
2022-01-31 08:33:12 +0300

    Add project files.

---
+++ BenchmarkDotNetExample.BenchmarkCalculating/StringVsStringBuilder.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Text;
+
+namespace BenchmarkDotNetExample.BenchmarkCalculating
+{
+    [SimpleJob(BenchmarkDotNet.Jobs.RuntimeMoniker.Net50, launchCount: 3, warmupCount: 5, targetCount: 3)]
+    [SimpleJob(BenchmarkDotNet.Jobs.RuntimeMoniker.Net60, launchCount: 3, warmupCount: 5, targetCount: 3)]
+    [MeanColumn, MedianColumn]
+    [MemoryDiagnoser]
+    public class StringVsStringBuilder
+    {
+        [Params(100, 1000, 10000)]
+        public int Count { get; set; }
+
+        [Benchmark]
+        public void StringTest()
+        {
+            string text = "";
+            for (int i = 0; i < Count; i++)
+            {
+                text += i.ToString();
+            }
+        }
+
+        [Benchmark]
+        public void StringBuilderTest()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < Count; i++)
+            {
+                stringBuilder.Append(i.ToString());
+            }
+        }
+    }
+}
---
+++ BenchmarkDotNetExample.StopwatchCalculating/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace BenchmarkDotNetExample.StopwatchCalculating
+{
+    class Program
+    {
+        static int count = 10000;
+        static void Main(string[] args)
+        {
+            Stopwatch stringTotalMiliSeconds = StringTest();
+            Stopwatch stringBuilderTotalMiliSeconds = StringBuilderTest();
+            Console.WriteLine("--------------------Stopwatch--------------------");
+            Console.WriteLine($"String: {stringTotalMiliSeconds.Elapsed} ms");
+            Console.WriteLine($"StringBuilder: {stringBuilderTotalMiliSeconds.Elapsed} ms");
+            Console.ReadLine();
+        }
+
+        private static Stopwatch StringTest()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            string text = "";
+            for (int i = 0; i < count; i++)
+            {
+                text += i.ToString();
+            }
+            stopwatch.Stop();
+            return stopwatch;
+        }
+
+        private static Stopwatch StringBuilderTest()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < count; i++)
+            {
+                stringBuilder.Append(i.ToString());
+            }
+            stopwatch.Stop();
+            return stopwatch;
+        }
+    }
+}
